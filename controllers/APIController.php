e8d65f194ce8e3714cb2b923dd6d8ee62222e245
Carlos Toledo <105874879+to1edo@users.noreply.github.com>
2022-07-07 01:21:18 -0400

    test API controller

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -16,18 +16,17 @@ class APIController{
 
     public static function guardar(){
         //almacena la cita y devuelve el id
+        echo "<pre>";
+        echo "test";
+        echo "</pre>";
 
         $cita = new Cita($_POST);
 
         echo "<pre>";
         var_dump($cita);
         echo "</pre>";
-        
-        $resultado = $cita->guardar();
 
-        echo "<pre>";
-        var_dump($resultado);
-        echo "</pre>";
+        $resultado = $cita->guardar();
 
         $id = $resultado['id'];
 
