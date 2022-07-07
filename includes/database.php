ffd57f32eeb0c1c4ab6a3f45cd310123c467220e
Carlos Toledo <105874879+to1edo@users.noreply.github.com>
2022-07-07 00:27:00 -0400

    test API controller

--- includes/database.php
+++ includes/database.php
@@ -1,7 +1,7 @@
 <?php
 
 $db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_BD']);
-
+mysqli_set_charset($db, 'utf8');
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
     echo "errno de depuración: " . mysqli_connect_errno();
